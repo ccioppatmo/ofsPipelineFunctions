f75bd13daf40dccf25fda11c27cf763adf94195e
ccioppaaccn <114012555+ccioppaaccn@users.noreply.github.com>
2023-01-04 09:17:05 -0500

    env

--- ADFFunctionOrchestrator/__init__.py
+++ ADFFunctionOrchestrator/__init__.py
@@ -6,15 +6,23 @@ import json
 
 import azure.functions as func
 import azure.durable_functions as df
+from ..shared_code.MyClasses import SerializableClass
 
+results = {}
+def orchestrator_function(context: df.DurableOrchestrationContext):
+    event: SerializableClass = context.get_input()
 
-def orchestrator_function(context: df.DurableOrchestrationContext, event):
-    event_data=json.loads(event_data)
-    task_handler = event_data['task_handler']['handler_name']
-    tasks = event_data['task_handler']['tasks']
-    for t in tasks:
-        tasks = []
-        tasks.append(context.call_activity(task_handler, t))
-    yield context.task_all(tasks)
-
+    if (event):
+        event: str = event.get_payload()
+        event_data = json.loads(event)
+        task_handler = event_data['task_handler']['handler_name']
+        tasks = event_data['task_handler']['tasks']
+        for t in tasks:
+            tasks = []
+            tasks.append(context.call_activity(task_handler, SerializableClass(5)))
+        results = yield context.task_all(tasks)
+    else:
+        results = None
+        yield results
+    return results
 main = df.Orchestrator.create(orchestrator_function)
