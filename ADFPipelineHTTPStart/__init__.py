2fbeb23c4c4c3b89fda0ad7acae63db85b11fc44
ccioppaaccn <114012555+ccioppaaccn@users.noreply.github.com>
2023-01-09 16:02:12 -0500

    Updates to httpstart

--- ADFPipelineHTTPStart/__init__.py
+++ ADFPipelineHTTPStart/__init__.py
@@ -13,7 +13,7 @@ async def main(req: func.HttpRequest, starter: str) -> func.HttpResponse:
     function_name = req.route_params['functionName']
     #orchestrator_payload = req.get_body()
     logging.log = (f'About to start ADFFunctionOrchestrator - Event Payload: {req}')
-    instance_id = await client.start_new(function_name, None, req)
+    instance_id = await client.start_new(function_name, None, None)
     logging.log = (f'ADFFunctionOrchestrator started with Instance ID : {instance_id}')
 
     return client.create_check_status_response(req, instance_id)
